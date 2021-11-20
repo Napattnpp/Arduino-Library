f8a4762d8248573a9614d85f2accfe47dbcc1ce1
Napattnpp <93720107+Napattnpp@users.noreply.github.com>
2021-11-20 23:31:58 +0700

    Update WiFi_tools.h

--- WiFi_tools/WiFi_tools.h
+++ WiFi_tools/WiFi_tools.h
@@ -1,1 +1,30 @@
+/*! v.0.0.1 !*/
 
+bool connectToWiFi(const char *ssid, const char *password){
+    byte ei = 0;
+
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+    Serial.print("\nConnecting to ");
+    Serial.println(ssid);
+    while (WiFi.status() != WL_CONNECTED){
+        Serial.print(".");
+        delay(500);
+        ei++;
+
+        //time out = 5 secound
+        if (ei == 10){
+            WiFi.disconnect();
+            Serial.print("\nCan't connect to ");
+            Serial.println(ssid);
+
+            return 0;
+        }
+    }
+    Serial.print("\nConnected to ");
+    Serial.println(WiFi.SSID());
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+
+    return 1;
+}
