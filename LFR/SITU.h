0342c272fc7845e94c2ec1b7f612c90c4479b10c
Napattnpp <93720107+Napattnpp@users.noreply.github.com>
2021-11-23 19:18:41 +0700

    Update SITU.h

--- LFR/SITU.h
+++ LFR/SITU.h
@@ -1,1 +1,24 @@
+#define oooo   ((sen1 > light) && (sen2 > light) && (sen3 > light) && (sen4 > light))  //ทุกตัวขาว
 
+#define oooi   ((sen1 > light) && (sen2 > light) && (sen3 > light) && (sen4 < light))  //0 0 0 1
+#define ooio   ((sen1 > light) && (sen2 > light) && (sen3 < light) && (sen4 > light))  //0 0 1 0
+#define ooii   ((sen1 > light) && (sen2 > light) && (sen3 < light) && (sen4 < light))  //0 0 1 1
+
+#define oioo   ((sen1 > light) && (sen2 < light) && (sen3 > light) && (sen4 > light))  //0 1 0 0
+#define oioi   ((sen1 > light) && (sen2 < light) && (sen3 > light) && (sen4 < light))  //0 1 0 1
+#define oiio   ((sen1 > light) && (sen2 < light) && (sen3 < light) && (sen4 > light))  //0 1 1 0
+#define oiii   ((sen1 > light) && (sen2 < light) && (sen3 < light) && (sen4 < light))  //0 1 1 1
+
+#define iooo   ((sen1 < light) && (sen2 > light) && (sen3 > light) && (sen4 > light))  //1 0 0 0
+#define iooi   ((sen1 < light) && (sen2 > light) && (sen3 > light) && (sen4 < light))  //1 0 0 1
+#define ioio   ((sen1 < light) && (sen2 > light) && (sen3 < light) && (sen4 > light))  //1 0 1 0
+#define ioii   ((sen1 < light) && (sen2 > light) && (sen3 < light) && (sen4 < light))  //1 0 1 1
+
+#define iioo  ((sen1 < light) && (sen2 < light) && (sen3 > light) && (sen4 > light))  //1 1 0 0
+#define iioi  ((sen1 < light) && (sen2 < light) && (sen3 > light) && (sen4 < light))  //1 1 0 1
+#define iiio  ((sen1 < light) && (sen2 < light) && (sen3 < light) && (sen4 > light))  //1 1 1 0
+
+#define iiii  ((sen1 < light) && (sen2 < light) && (sen3 < light) && (sen4 < light))  //ทุกตัวดำ
+
+/*! ดำ = 16, 17 !*/
+/*! ขาว = 38 !*/
