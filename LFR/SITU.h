ae0664e5d73d8ca9d6fd3294e4417fec0afe9ca4
Napattnpp <93720107+Napattnpp@users.noreply.github.com>
2021-11-25 16:08:04 +0700

    Update SITU.h

--- LFR/SITU.h
+++ LFR/SITU.h
@@ -1,1 +1,28 @@
+/*
+*  Situations
+*  4 sensors
+*  sen. > light = o
+*  sen. < light = i
+*/
 
+#define oooo   ((sen1 > light) && (sen2 > light) && (sen3 > light) && (sen4 > light))  //all white
+
+#define oooi   ((sen1 > light) && (sen2 > light) && (sen3 > light) && (sen4 < light))  //0 0 0 1
+#define ooio   ((sen1 > light) && (sen2 > light) && (sen3 < light) && (sen4 > light))  //0 0 1 0
+#define ooii   ((sen1 > light) && (sen2 > light) && (sen3 < light) && (sen4 < light))  //0 0 1 1
+
+#define oioo   ((sen1 > light) && (sen2 < light) && (sen3 > light) && (sen4 > light))  //0 1 0 0
+#define oioi   ((sen1 > light) && (sen2 < light) && (sen3 > light) && (sen4 < light))  //0 1 0 1
+#define oiio   ((sen1 > light) && (sen2 < light) && (sen3 < light) && (sen4 > light))  //0 1 1 0
+#define oiii   ((sen1 > light) && (sen2 < light) && (sen3 < light) && (sen4 < light))  //0 1 1 1
+
+#define iooo   ((sen1 < light) && (sen2 > light) && (sen3 > light) && (sen4 > light))  //1 0 0 0
+#define iooi   ((sen1 < light) && (sen2 > light) && (sen3 > light) && (sen4 < light))  //1 0 0 1
+#define ioio   ((sen1 < light) && (sen2 > light) && (sen3 < light) && (sen4 > light))  //1 0 1 0
+#define ioii   ((sen1 < light) && (sen2 > light) && (sen3 < light) && (sen4 < light))  //1 0 1 1
+
+#define iioo  ((sen1 < light) && (sen2 < light) && (sen3 > light) && (sen4 > light))  //1 1 0 0
+#define iioi  ((sen1 < light) && (sen2 < light) && (sen3 > light) && (sen4 < light))  //1 1 0 1
+#define iiio  ((sen1 < light) && (sen2 < light) && (sen3 < light) && (sen4 > light))  //1 1 1 0
+
+#define iiii  ((sen1 < light) && (sen2 < light) && (sen3 < light) && (sen4 < light))  //all black
